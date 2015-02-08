582ce7525c9a157af2d4f9e85cdc2a2e77b7b298
Sam Tran <samvtran@gmail.com>
2015-02-08 15:19:50 -0800

    Initial Commit

---
+++ MatchDelimiters/MatchDelimiters.swift
@@ -0,0 +1,48 @@
+//
+//  MatchDelimiters.swift
+//
+//  Created by Sam Tran on 2/8/15.
+//  Copyright (c) 2015 Sam Tran. All rights reserved.
+//
+
+import AppKit
+
+var sharedPlugin: MatchDelimiters?
+
+class MatchDelimiters: NSObject {
+    var bundle: NSBundle
+
+    class func pluginDidLoad(bundle: NSBundle) {
+        let appName = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as? NSString
+        if appName == "Xcode" {
+            sharedPlugin = MatchDelimiters(bundle: bundle)
+        }
+    }
+
+    init(bundle: NSBundle) {
+        self.bundle = bundle
+
+        super.init()
+        createMenuItems()
+    }
+
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    func createMenuItems() {
+        var item = NSApp.mainMenu!!.itemWithTitle("Edit")
+        if item != nil {
+            var actionMenuItem = NSMenuItem(title:"Do Action", action:"doMenuAction", keyEquivalent:"")
+            actionMenuItem.target = self
+            item!.submenu!.addItem(NSMenuItem.separatorItem())
+            item!.submenu!.addItem(actionMenuItem)
+        }
+    }
+
+    func doMenuAction() {
+        let error = NSError(domain: "Hello World!", code:42, userInfo:nil)
+        NSAlert(error: error).runModal()
+    }
+}
+
